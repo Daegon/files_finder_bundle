37446062bb9595b8f1c441222c8b80a3c8b3dbb8
Zimm <Romsssa@gmail.com>
2016-03-05 12:55:33 +0200

    Start test fixtures

---
+++ Tests/Fixtures/app/AppKernel.php
@@ -0,0 +1,34 @@
+<?php
+
+use Symfony\Component\HttpKernel\Kernel;
+use Symfony\Component\Config\Loader\LoaderInterface;
+
+class AppKernel extends Kernel
+{
+    public function registerBundles()
+    {
+        $bundles = array(
+            new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
+            new Symfony\Bundle\TwigBundle\TwigBundle(),
+            new Symfony\Bundle\MonologBundle\MonologBundle(),
+            new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+            new JMS\SerializerBundle\JMSSerializerBundle(),
+            new FOS\RestBundle\FOSRestBundle(),
+            new Nelmio\ApiDocBundle\NelmioApiDocBundle(),
+            new Massmedia\FilesFinderBundle\FilesFinderBundle(),
+            new Bazinga\Bundle\RestExtraBundle\BazingaRestExtraBundle()
+        );
+
+        return $bundles;
+    }
+
+    public function getRootDir()
+    {
+        return __DIR__;
+    }
+
+    public function registerContainerConfiguration(LoaderInterface $loader)
+    {
+        $loader->load(__DIR__.'/config/default.yml');
+    }
+}
